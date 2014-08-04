419c011678c0cf1bd13f1ebb27e102deb101e9db
sely2k <sely2k@gmail.com>
2014-08-05 01:27:43 +0200

    primo

---
+++ BikeR.Web/Configuration/AuthApiKeySection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace BikeR.Web.Configuration
+{
+    public class AuthApiKeySection : ConfigurationSection
+    {
+
+        // Create a "font" element.
+        [ConfigurationProperty("GoogleApi")]
+        public GoogleApiKey GoogleApi
+        {
+            get
+            {
+                return (GoogleApiKey)this["GoogleApi"];
+            }
+            set
+            { this["GoogleApi"] = value; }
+        }
+
+
+    }
+}
---
+++ BikeR.Web/Configuration/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BikeR.Web.Configuration
+{
+    public class Class1
+    {
+    }
+}
