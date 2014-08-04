62a520a593e259916b70a4b5167a279547ee6132
sely2k <sely2k@gmail.com>
2014-08-05 01:17:06 +0200

    first checkin

---
+++ BikeR.MobileServices/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BikeR.MobileServices.WebApiApplication" Language="C#" %>
---
+++ BikeR.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BikeR.Web.Startup))]
+namespace BikeR.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
