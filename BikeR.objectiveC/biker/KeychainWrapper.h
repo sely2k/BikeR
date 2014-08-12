79d221c1dca40bf2b3851ea56a4df4ba7e130246
Leonardo D'Ottavi <leonardo.dottavi@wfp.org>
2014-08-13 01:03:07 +0200

    first iOs Client

---
+++ BikeR.objectiveC/biker/KeychainWrapper.h
@@ -0,0 +1,13 @@
+//
+//  KeychainWrapper.h
+//  biker
+//
+//  Created by Leonardo D'Ottavi on 13/08/14.
+//  Copyright (c) 2014 MobileServices. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KeychainWrapper : NSObject
+
+@end
