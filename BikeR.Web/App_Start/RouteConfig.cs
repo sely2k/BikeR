15ffa907ee2ded1f8e75c7abd25fe0400fd9110e
sely2k <sely2k@gmail.com>
2014-09-02 07:39:51 +0200

    home page

--- BikeR.Web/App_Start/RouteConfig.cs
+++ BikeR.Web/App_Start/RouteConfig.cs
@@ -18,6 +18,11 @@ namespace BikeR.Web
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                 name: "DownloadApp",
+                 url: "{controller}/{action}/{name}/{id}"
+             );
         }
     }
 }
